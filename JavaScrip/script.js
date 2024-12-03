242a1309748a43e4a6208eef9799f8cfb44e66ae
l6712 <161103594+l6712@users.noreply.github.com>
2024-12-02 19:23:19 -0800

    Update script.js

--- JavaScrip/script.js
+++ JavaScrip/script.js
@@ -39,3 +39,19 @@ document.getElementById("loginForm").addEventListener("submit", function(event) 
         document.getElementById("error-message").textContent = "Todos los campos son obligatorios.";
     }
 });
+
+//------------------------------------------------------------------
+
+    document.getElementById('search-bar').addEventListener('input', function() {
+        var searchQuery = this.value.toLowerCase();
+        var products = document.querySelectorAll('.producto');
+
+        products.forEach(function(product) {
+            var productName = product.getElementsByTagName('h3')[0].textContent.toLowerCase();
+            if (productName.includes(searchQuery)) {
+                product.style.display = 'block';
+            } else {
+                product.style.display = 'none';
+            }
+        });
+    });
