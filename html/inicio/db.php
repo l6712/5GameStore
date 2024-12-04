886ff920e612dcaaf8b32d7e9fefadcb26b0f825
l6712 <161103594+l6712@users.noreply.github.com>
2024-12-03 23:50:30 -0800

    Add files via upload

---
+++ html/inicio/db.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "5gamestore";
+
+try {
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    if ($conn->connect_error) {
+        throw new Exception("Connection failed: " . $conn->connect_error);
+    }
+} catch (Exception $e) {
+    die($e->getMessage());
+}
+?>
